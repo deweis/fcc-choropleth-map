10878db631324ea777eac334a3a13fce36baf5b1
Dennis <deweis@gmx.ch>
2018-12-30 11:19:04 +0100

    Update tasks / examples on script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+Examples Check:
+- https://www.google.com/search?client=firefox-b-ab&q=d3+Choropleth+map
+
+Results:
+- https://www.youtube.com/watch?v=OoZ0LWD9KUs
+- https://bl.ocks.org/JulienAssouline/1ae3480c5277e2eecd34b71515783d6f
+- http://bl.ocks.org/palewire/d2906de347a160f38bc0b7ca57721328
+- https://beta.observablehq.com/@mbostock/d3-choropleth
+
+TBD:
+- Check on examples
+- Fetch the data
+
+*/
